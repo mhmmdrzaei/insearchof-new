0d0792652800e8defaa8dbd3754994c1791239fa
Mohammad Rezaei <mhmmdrzaei@gmail.com>
2023-11-02 10:40:15 -0400

    SEO changes

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -12,18 +12,18 @@ const settings = getsettings();
 
 
 export const metadata = {
-  title: 'In Search Of',
+  title: 'In Search Of | full service casting & management agency',
   description: 'In Search Of is a full service casting and management agency based in New York, Los Angeles + Toronto. The agency acts as a social archive, sharing stories of the often overlooked, documenting cultural moments, and observing the relationships between an individual and a shared collective experience.',
   openGraph: {
-    title: 'In Search Of',
+    title: 'In Search Of | full service casting & management agency',
     description: 'In Search Of is a full service casting and management agency based in New York, Los Angeles + Toronto. The agency acts as a social archive, sharing stories of the often overlooked, documenting cultural moments, and observing the relationships between an individual and a shared collective experience.',
     url: 'https://insearchof.agency',
-    siteName: 'Next.js',
+    siteName: 'In Search Of | full service casting & management agency',
     images: [
       {
-        url: 'https://cdn.sanity.io/images/5zyxoqnf/production/031e1b5398d0b78c3c1ba1c3e2bc240227338de3-1200x626.png',
+        url: 'https://cdn.sanity.io/images/5zyxoqnf/production/2a905d4706960f2a5988a7f6e09e1554e34d88cf-1200x627.jpg',
         width: 1200,
-        height: 626,
+        height: 627,
       },
     ],
     locale: 'en_US',
